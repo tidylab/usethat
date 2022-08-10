0095c47b9db37baa6f23f71abe44c20a1fa6d5f6
harell <harell@users.noreply.github.com>
2022-08-10 14:31:09 +1200

    correction

--- tests/testthat/test-add_value_object.R
+++ tests/testthat/test-add_value_object.R
@@ -1,5 +1,5 @@
 test_that("add_value_object creates a value object", {
     usethis::with_project(path = path, {
-        expect_no_error(add_value_object(obj = mtcars, domain = NULL, name = "Car"))
+        expect_no_error(add_value_object(obj = mtcars, name = "Car", domain = NULL))
     })
 })
