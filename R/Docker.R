b3ad44d1ec595e3b43f1e9994f07c5336f78991c
harell <harell@users.noreply.github.com>
2022-08-11 14:20:10 +1200

    is_docker_installed and is_docker_running

--- R/Docker.R
+++ R/Docker.R
@@ -6,9 +6,13 @@
 #' docker <- Docker$new()
 Docker <- R6::R6Class(classname = "Docker", cloneable = FALSE, public = list(
     #' @description Instantiate a Docker Object
-    initialize = function() private$.initialize()
+    initialize = function() private$.initialize(),
+    #' @description Check if Docker is installed
+    is_docker_installed = function() private$.is_docker_installed(),
+    #' @description Check if Docker is running
+    is_docker_running = function() private$.is_docker_running()
 ), private = list(
-    .initialize = function() stop("function not implemented")
+    system_quietly = function(command, ...) base::system(command, ..., ignore.stderr = TRUE, ignore.stdout = TRUE)
 ))
 
 
@@ -17,3 +21,10 @@ Docker$set("private", ".initialize", overwrite = TRUE, function(){
     NULL
 })#.initialize
 
+Docker$set("private", ".is_docker_installed", overwrite = TRUE, function(){
+    invisible(isTRUE(grep("Docker version", private$system_quietly("docker --version", intern = TRUE)) == 1))
+})#.is_docker_installed
+
+Docker$set("private", ".is_docker_running", overwrite = TRUE, function(){
+    invisible(!isTRUE(private$system_quietly("docker stats --no-stream") == 1))
+})#.is_docker_running
--- tests/testthat/test-Docker.R
+++ tests/testthat/test-Docker.R
@@ -1,4 +1,6 @@
 test_that("Docker works", {
-    docker <- Docker$new()
-    skip("Requires Docker runnings")
+    expect_no_error(docker <- Docker$new())
+    expect_no_error(docker$is_docker_installed())
+    expect_no_error(docker$is_docker_running())
+    skip_if_not(docker$is_docker_running(), "Docker is not running")
 })
